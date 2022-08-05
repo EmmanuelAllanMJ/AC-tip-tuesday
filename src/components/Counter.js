149f468fedcf470fcc4300383a67abb718088ce7
Emmanuel Allan <emmanuelallanmj@gmail.com>
2022-08-05 19:59:15 +0530

    Class Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Counter() {
-  return <div>Counter</div>;
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increase() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Counter App using Class Component : </h3>
+        <h2> {this.state.count}</h2>
+        <button onClick={this.increase.bind(this)}> Add</button>
+      </div>
+    );
+  }
 }
 
 export default Counter;
